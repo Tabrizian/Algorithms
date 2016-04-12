aec5cb2182af182f1b5aa9d2a4ff144400239af5
Iman Tabrizian <tabrizian@outlook.com>
2016-04-12 23:14:19 +0430

    Adds tsp prototype.

---
+++ tsp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int tsp(int *mass, int *value, int size){
+}
+
+int main() {
+
+    int mass[] = {2, 10, 5, 13};
+    int value[] = {123, 14, 43, 21};
+    return 0;
+}
