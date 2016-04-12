8cb9da2b0e8d0bac52f09291180674e6e748c618
Iman Tabrizian <tabrizian@outlook.com>
2016-04-12 22:42:55 +0430

    Adds sequenc.cpp not working

--- sequence.cpp
+++ sequence.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#define IN_DEL 2 //Insertion or deleting
+#define MUT 1    //Mutation cost
 using namespace std;
 
 int seq_align(int i, int j, int arr1[], int arr2[], int size1, int size2){
     if(i == size1 )
-        return 2*(size2-j +1);
+        return 2 * (size2 - j);
     else if(j == size2 )
-        return 2*(size1-i+1);
+        return 2 * (size1 - i);
     else
     {
-        int first = (arr1[i] == arr2[j]) ? 0 : 1 + seq_align(i + 1, j + 1,arr1,
-                arr2,size1,size2);
-        int second = 2 + seq_align(i, j + 1, arr1, arr2, size1, size2);
-        int third = 2 + seq_align(i + 1, j, arr1, arr2, size1, size2);
+        int first = (arr1[i] == arr2[j]) ? 0 : MUT + seq_align(i + 1, j + 1, arr1,
+                arr2, size1, size2);
+        int second = IN_DEL + seq_align(i, j + 1, arr1, arr2, size1, size2);
+        int third = IN_DEL + seq_align(i + 1, j, arr1, arr2, size1, size2);
 
         int min = first;
         if(second < min)
@@ -19,6 +21,11 @@ int seq_align(int i, int j, int arr1[], int arr2[], int size1, int size2){
         if(third < min)
             min = third;
 
+        cout << "i: " << i << " j: " << j << endl;
+        cout << "arr1[i] " << arr1[i] << " arr2[j] " << arr2[j] << endl;
+        cout << "First: " << first << endl;
+        cout << "Second: " << second << endl;
+        cout << "Third: " << third << endl;
         return min;
     }
 
@@ -28,7 +35,7 @@ int main(){
     int arr1[] = {1,2,3,2};
     int arr2[] = {3,2,4,2};
 
-    cout<<seq_align(0,0,arr1,arr2,4,4);
+    cout<<seq_align(0 , 0, arr1, arr2, 4, 4);
 
     return 0;
 }
